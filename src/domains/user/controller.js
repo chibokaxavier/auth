ecd85eaa6be09833e785659ae41e06884bbc616e
manlikexavy <chibokaxavier@gmail.com>
2024-08-05 22:01:40 +0100

    endpoint created(but route hasnt been exposed to the app for full use)

---
+++ src/domains/user/controller.js
@@ -0,0 +1,25 @@
+const User = require("./model");
+const { hashData } = require("../../utils/hashData");
+
+const createNewUser = async (data) => {
+  try {
+    const { name, email, password } = data;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw Error("User with the provided email already exists");
+    }
+    const hashedPassword = await hashData(password);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const createdUser = await newUser.save();
+    return createdUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { createNewUser };
---
+++ src/domains/user/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const createNewUser = require("./controller");
+
+router.post("/signup", async (req, res) => {
+  try {
+    let { name, email, password } = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    if (!(name && email && password)) {
+      throw Error("Empty input fields!");
+    } else if (!/^[a-zA-Z]*$/.test(name)) {
+      throw Error("Invalid name entered!");
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      throw Error("Invalid email entered!");
+    } else if (password.length < 8) {
+      throw Error("Password should be at least 8 characters long");
+    } else {
+      const newUser = await createNewUser({ name, email, password });
+      res.status(200).json(newUser);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
